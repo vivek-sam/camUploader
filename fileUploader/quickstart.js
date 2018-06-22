0f7e7632150e49ec45297b3c4fd3d53aa2753c63
Vivek Sam <viveksam@gmail.com>
2018-06-22 14:48:47 +0530

    fixed error

--- fileUploader/quickstart.js
+++ fileUploader/quickstart.js
@@ -135,7 +135,7 @@ if (fs.existsSync(lock1)) {
       
       logger.info("File : " + fullpath);
       // upload this file.... 
-      var filename = path.basename(fullepath);
+      var filename = path.basename(fullpath);
       var mType = 'video/H264';
       if(path.extname(filename) === 'jpg') {
         mType = 'image/jpeg';
